c2be40fb90a3ce809a2506fd9a6393331d3b4226
Tabesh2020 <tabesh2020@users.noreply.github.com>
2025-10-12 19:17:00 +0200

    Rollback release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="manim-fa",
-    version="0.3.3",
+    version="0.3.1.post1",
     packages=find_packages(),
     install_requires=[
         "manim>=0.17.0",
